7353fefb148d6bb7617cd95a4584122887dedba2
xiaolin <462441355@qq.com>
2017-12-29 16:32:32 +0800

    * edit

--- app/Console/Socket.php
+++ app/Console/Socket.php
@@ -1,11 +1,4 @@
 <?php
-// +----------------------------------------------------------------------
-// | WebSocket 抽象类 [ WE CAN DO IT JUST THINK IT ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
-// +----------------------------------------------------------------------
-// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
-// +----------------------------------------------------------------------
 namespace App\Console;
 
 use Illuminate\Console\Command;
